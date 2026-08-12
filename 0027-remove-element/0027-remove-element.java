7697cb45ee14124a624306e444c870cdcd247f48
Vaishali V Moolya <165879651+VaishaliVMoolya@users.noreply.github.com>
2026-08-12 18:44:17 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.63%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int read = 0;
+        int write = 0;
+        while(read<nums.length){
+            if(nums[read] != val){
+                    nums[write] = nums[read];
+                    write++;
+            }
+            read++;
+        }
+        return write;
+    }
+}
